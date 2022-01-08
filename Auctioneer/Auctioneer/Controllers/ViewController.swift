f47531644a6566e4b3059d35c962e3b0e8542abe
Lester Cheong <73060774+s10208233@users.noreply.github.com>
2022-01-09 00:39:54 +0800

    Initial Push
    Setup

---
+++ Auctioneer/Auctioneer/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Auctioneer
+//
+//  Created by Lester Cheong on 9/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
