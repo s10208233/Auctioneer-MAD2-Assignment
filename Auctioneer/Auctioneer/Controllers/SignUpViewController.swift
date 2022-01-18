e88551a57ef0f6a65147d67a6300401658df13f2
Chow Keng Shion <s10204765@connect.np.edu.sg>
2022-01-18 14:14:38 +0800

    start signup viewcontroller

---
+++ Auctioneer/Auctioneer/Controllers/SignUpViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SignUpViewController.swift
+//  Auctioneer
+//
+//  Created by Shion on 18/1/22.
+//
+
+import Foundation
+import Firebase
+
+class SignUpViewController: UIViewController, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var newUsername: UITextField!
+    @IBOutlet weak var newPassword: UITextField!
+    
+    @IBAction func signUpButton(_ sender: Any) {
+        
+    }
+    
+}
