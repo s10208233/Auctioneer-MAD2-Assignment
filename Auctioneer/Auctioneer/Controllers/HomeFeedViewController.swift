402ba99e9aaa5c214947a5f02092bd3d584ebb50
Chow Keng Shion <s10204765@connect.np.edu.sg>
2022-01-20 22:35:36 +0800

    major breakthrough home feed i can finally eat

--- Auctioneer/Auctioneer/Controllers/HomeFeedViewController.swift
+++ Auctioneer/Auctioneer/Controllers/HomeFeedViewController.swift
@@ -11,7 +11,9 @@ import UIKit
 
 class HomeFeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     let appdelegate = (UIApplication.shared.delegate) as! AppDelegate
-
+    var returndict:[String:AuctionItem] = [:]
+    var productList:[AuctionItem] = []
+    
     let testitems = [
 
     AuctionItem(
@@ -43,23 +45,31 @@ class HomeFeedViewController: UIViewController, UITableViewDelegate, UITableView
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         HomeFeedTableView.delegate = self
         HomeFeedTableView.dataSource = self
-        
+        retrieveData()
+    }
+    
+    func retrieveData(){
         let formatter = DateFormatter()
         formatter.dateStyle = .short
-        var returndict:[String:AuctionItem] = [:]
+
         let ref = Database.database().reference()
         ref.child("Products").observeSingleEvent(of: .value){ (snapshot) in
             let products = snapshot.value as! [String:Dictionary<String, Any>]
             products.forEach{ pairs in()
-                returndict.updateValue(AuctionItem(productname: pairs.value["productname"] as! String, imageurl: pairs.value["imageurl"] as! String, openedby: pairs.value["openby"] as! String, opendate: formatter.date(from:pairs.value["opendate"] as! String)!, closedate: formatter.date(from:pairs.value["closedate"] as! String)!, startingprice: pairs.value["startingprice"] as! Double, highestbidprice: pairs.value["highestbidprice"] as! Double, highestbidder: pairs.value["highestbidder"] as! String), forKey: pairs.key)
+                self.returndict.updateValue(AuctionItem(productname: pairs.value["productname"] as! String, imageurl: pairs.value["imageurl"] as! String, openedby: pairs.value["openby"] as! String, opendate: formatter.date(from:pairs.value["opendate"] as! String)!, closedate: formatter.date(from:pairs.value["closedate"] as! String)!, startingprice: pairs.value["startingprice"] as! Double, highestbidprice: pairs.value["highestbidprice"] as! Double, highestbidder: pairs.value["highestbidder"] as! String), forKey: pairs.key)
             }
-            print("outside\(returndict)")
-            print(self.appdelegate.SignedIn_UserKey!)            
+            
+            for (key, value) in self.returndict {
+                self.productList.append(AuctionItem(productname: value.productName, imageurl: value.imageUrl, openedby: value.openedBy, opendate: value.openDate, closedate: value.closeDate, startingprice: value.startingPrice, highestbidprice: value.highestBidPrice, highestbidder: value.highestBidder))
+            }
+            print("outside\(self.returndict)")
+            self.HomeFeedTableView.reloadData()
+           
         }
     }
+
     
     //  TableView Cell bindings
     func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
@@ -68,12 +78,27 @@ class HomeFeedViewController: UIViewController, UITableViewDelegate, UITableView
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return testitems.count
+        return productList.count
     }
     
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = HomeFeedTableView.dequeueReusableCell(withIdentifier: "HFCustomCell") as! HomeFeedCustomCell
+//        let AuctionItem = testitems[indexPath.row]
+//
+//        //  Cell Binding
+//        if let url = URL(string: AuctionItem.imageUrl){
+//            cell.Cell_ImageView.loadImage(from: url)
+//        }
+////        cell.Cell_ImageView.loadImage(from: URL(string: AuctionItem.imageUrl) as! URL)
+////        cell.Cell_ImageView.load(urlString: AuctionItem.imageUrl)
+//        cell.Cell_ItemNameLabel.text = AuctionItem.productName
+//        cell.Cell_StartingPriceLabel.text? = "Starting at $\(String(format: "%.2f", AuctionItem.startingPrice))"
+//        cell.Cell_NowAtPrice_Label.text? = "Currently at $\(String(format: "%.2f", AuctionItem.highestBidPrice))"
+//        return cell
+//    }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = HomeFeedTableView.dequeueReusableCell(withIdentifier: "HFCustomCell") as! HomeFeedCustomCell
-        let AuctionItem = testitems[indexPath.row]
+        let AuctionItem = productList[indexPath.row]
         
         //  Cell Binding
         if let url = URL(string: AuctionItem.imageUrl){
