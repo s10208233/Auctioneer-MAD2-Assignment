0cf6028edfd88f5ed16a097544d5732c8214e8d0
Lester Cheong <73060774+s10208233@users.noreply.github.com>
2022-01-21 04:46:03 +0800

    lol

--- Auctioneer/Auctioneer/CustomImageView.swift
+++ Auctioneer/Auctioneer/CustomImageView.swift
@@ -8,9 +8,25 @@
 import Foundation
 import UIKit
 
+let imageCache = NSCache<AnyObject, AnyObject>()
+
 class CustomImageView : UIImageView {
+    var task:URLSessionDataTask!
+    let spinner = UIActivityIndicatorView(style: .gray)
     
     func loadImage(from url: URL){
+        image = nil
+        spinner.startAnimating()
+
+        
+        if let task = task {
+            task.cancel()
+        }
+        
+        if let imageFromCache = imageCache.object(forKey: url.absoluteString as AnyObject) as? UIImage {
+            self.image = imageFromCache
+        }
+        
             let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
                 guard
                     let data = data,
@@ -19,11 +35,20 @@ class CustomImageView : UIImageView {
                     print("Couldn't Load Image From URL: \(url)")
                     return
                 }
+                
+                imageCache.setObject(newImage, forKey: url.absoluteString as AnyObject)
+                
                 DispatchQueue.main.async {
                     self.image = newImage
                 }
         }
         task.resume()
+    }
+    
+    func addSpinner() {
+        spinner.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        spinner.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        spinner.startAnimating()
     }
     
 }
