614c60dcef372f7e7a029f61fffdec4347994631
Chow Keng Shion <s10204765@connect.np.edu.sg>
2022-01-20 20:09:02 +0800

    homeFeedViewController DAL

--- Auctioneer/Auctioneer/Controllers/HomeFeedViewController.swift
+++ Auctioneer/Auctioneer/Controllers/HomeFeedViewController.swift
@@ -14,13 +14,14 @@ class HomeFeedViewController: UIViewController{
     let appdelegate = (UIApplication.shared.delegate) as! AppDelegate
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        var returndict:[String:User] = [:]
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        var returndict:[String:AuctionItem] = [:]
         let ref = Database.database().reference()
-        ref.child("Users").observeSingleEvent(of: .value){ (snapshot) in
-            let users = snapshot.value as! [String:Dictionary<String, Any>]
-            users.forEach{ pairs in
-                returndict.updateValue(User(Username: pairs.value["Username"] as! String, Password:pairs.value["Password"] as! String), forKey: pairs.key)
+        ref.child("Products").observeSingleEvent(of: .value){ (snapshot) in
+            let products = snapshot.value as! [String:Dictionary<String, Any>]
+            products.forEach{ pairs in()
+                returndict.updateValue(AuctionItem(productname: pairs.value["productname"] as! String, imageurl: pairs.value["imageurl"] as! String, openedby: pairs.value["openby"] as! String, opendate: formatter.date(from:pairs.value["opendate"] as! String)!, closedate: formatter.date(from:pairs.value["closedate"] as! String)!, startingprice: pairs.value["startingprice"] as! Double, highestbidprice: pairs.value["highestbidprice"] as! Double, highestbidder: pairs.value["highestbidder"] as! String), forKey: pairs.key)
             }
             print("outside\(returndict)")
             print(self.appdelegate.SignedIn_UserKey!)
