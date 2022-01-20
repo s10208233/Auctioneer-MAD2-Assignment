1b3df56f4759b1b985f838e4d0d10e12c69ab4ae
Chow Keng Shion <s10204765@connect.np.edu.sg>
2022-01-20 21:43:06 +0800

    added signup validation

--- Auctioneer/Auctioneer/Controllers/SignUpViewController.swift
+++ Auctioneer/Auctioneer/Controllers/SignUpViewController.swift
@@ -22,6 +22,7 @@ class SignUpViewController: UIViewController {
     @IBOutlet weak var newPassword: UITextField!
     
     @IBAction func signUpButton(_ sender: Any) {
+        var bool:Bool = true
         //  Missing Input Validation
         if (newUsername.text == nil || newUsername.text == "") {
             let alert = UIAlertController(title: "Missing Username", message: "The username field is empty, please make sure that the username and password are filled.", preferredStyle: .alert)
@@ -35,18 +36,39 @@ class SignUpViewController: UIViewController {
         }
         
         let ref = Database.database().reference()
+        ref.child("Users").observeSingleEvent(of: .value){
+            (snapshot) in
+            let users = snapshot.value as? [String:Dictionary<String, Any>]
+            users?.forEach{ pairs in
+                if (pairs.value["Username"] as! String == self.newUsername.text!)
+                {
+                    let alert = UIAlertController(title: "Signup Failed", message: "The name you have entereted already exist, Please enter another name.", preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "Confirm", style: .default))
+                    self.present(alert, animated: true, completion: nil)
+                    bool = false
+                }
+            }
+            if (bool){
+                self.user = User(Username: self.newUsername.text!, Password: self.newPassword.text!)
+                ref.child("Users/").childByAutoId().setValue(["Username":self.newUsername.text!,"Password":self.newPassword.text!])
+                //  Prompt users that login is sucessful and redirect to login page
+                let alert = UIAlertController(title: "Success", message: "Your account has been created! Login now to start using Auctioneer!", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Confirm", style: .default, handler: {[weak alert] (_) in
+                    let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                    let vc = storyboard.instantiateViewController(withIdentifier: "LoginPage") as UIViewController
+                    vc.modalPresentationStyle = .fullScreen // try without fullscreen?
+                    self.present(vc, animated: true, completion: nil)
+                }))
+                self.present(alert, animated: true)
+            }
+        }
         
-        user = User(Username: newUsername.text!, Password: newPassword.text!)
-        ref.child("Users/").childByAutoId().setValue(["Username":newUsername.text!,"Password":newPassword.text!])
-        //  Prompt users that login is sucessful and redirect to login page
-        let alert = UIAlertController(title: "Success", message: "Your account has been created! Login now to start using Auctioneer!", preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "Confirm", style: .default, handler: {[weak alert] (_) in
-            let storyboard = UIStoryboard(name: "Main", bundle: nil)
-            let vc = storyboard.instantiateViewController(withIdentifier: "LoginPage") as UIViewController
-            vc.modalPresentationStyle = .fullScreen // try without fullscreen?
-            self.present(vc, animated: true, completion: nil)
-        }))
-        present(alert, animated: true)
+
+        
+        
+        
+        
+
     }
     
 }
