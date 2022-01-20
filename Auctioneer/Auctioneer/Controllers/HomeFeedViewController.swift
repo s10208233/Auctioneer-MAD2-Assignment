6ba66d7c8ee2ed405da3bc04c79443ffd624af04
Lester Cheong <73060774+s10208233@users.noreply.github.com>
2022-01-20 20:16:28 +0800

    Home Feed: Custom Cell View, UITableView with custom cells, async image loading

--- Auctioneer/Auctioneer/Controllers/HomeFeedViewController.swift
+++ Auctioneer/Auctioneer/Controllers/HomeFeedViewController.swift
@@ -9,14 +9,69 @@ import Foundation
 import FirebaseDatabase
 import UIKit
 
-class HomeFeedViewController: UIViewController{
+class HomeFeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    let testitems = [
 
+    AuctionItem(
+        productname: "This Cat",
+        imageurl: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/domestic-cat-lies-in-a-basket-with-a-knitted-royalty-free-image-1592337336.jpg",
+        openedby: "SampleUsername",
+        opendate: Date(), closedate: Date(),
+        startingprice: 1.00, highestbidprice: 0.0,
+        highestbidder: "AnotherSampleUsername")
+    ,
+    AuctionItem(
+        productname: "This Cat",
+        imageurl: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/domestic-cat-lies-in-a-basket-with-a-knitted-royalty-free-image-1592337336.jpg",
+        openedby: "SampleUsername",
+        opendate: Date(), closedate: Date(),
+        startingprice: 1.00, highestbidprice: 0.0,
+        highestbidder: "AnotherSampleUsername")
+    ,
+    AuctionItem(
+        productname: "This Cat",
+        imageurl: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/domestic-cat-lies-in-a-basket-with-a-knitted-royalty-free-image-1592337336.jpg",
+        openedby: "SampleUsername",
+        opendate: Date(), closedate: Date(),
+        startingprice: 1.00, highestbidprice: 0.0,
+        highestbidder: "AnotherSampleUsername")
+    ]
+    
+    @IBOutlet weak var HomeFeedTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        HomeFeedTableView.delegate = self
+        HomeFeedTableView.dataSource = self
     }
     
+    //  TableView Cell bindings
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        //  Cell Height
+        return 165
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return testitems.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = HomeFeedTableView.dequeueReusableCell(withIdentifier: "HFCustomCell") as! HomeFeedCustomCell
+        let AuctionItem = testitems[indexPath.row]
+        
+        //  Cell Binding
+        if let url = URL(string: AuctionItem.imageUrl){
+            cell.Cell_ImageView.loadImage(from: url)
+        }
+//        cell.Cell_ImageView.loadImage(from: URL(string: AuctionItem.imageUrl) as! URL)
+//        cell.Cell_ImageView.load(urlString: AuctionItem.imageUrl)
+        cell.Cell_ItemNameLabel.text = AuctionItem.productName
+        cell.Cell_StartingPriceLabel.text? = "Starting at $\(String(format: "%.2f", AuctionItem.startingPrice))"
+        cell.Cell_NowAtPrice_Label.text? = "Currently at $\(String(format: "%.2f", AuctionItem.highestBidPrice))"
+        return cell
+    }
 
     
     func retrieveData()->[String:User]{
